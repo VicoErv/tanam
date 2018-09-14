266a328394d62153f4495d24b97f964351660039
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-09-14 09:23:39 +0800

    add initializeApp and adminnew route

--- src/app.ts
+++ src/app.ts
@@ -20,15 +20,35 @@ export function initializeApp(tanamConfig: TanamConfig = {}) {
   const adminClientDir = path.join(__dirname, 'admin_client');
   const appConfig = { ...tanamDefaultAppConfig, ...(tanamConfig || {}) };
 
+  if (admin.apps.length === 0) {
+    admin.initializeApp();
+  }
+
   admin.firestore().settings({ timestampsInSnapshots: true });
 
-  app.use(`/${appConfig.adminUrl}/public`, express.static(path.join(adminClientDir, 'public')));
-  app.all(`/${appConfig.adminUrl}`, (req: express.Request, res: express.Response) => {
-    res.redirect('/admin/');
-  });
-  app.use(`/${appConfig.adminUrl}/**`, (req: express.Request, res: express.Response) => {
-    handlers.handleAdminPage(res, adminClientDir, tanamConfig.firebaseConfig);
-  });
+  app.use(
+    `/${appConfig.adminUrl}/public`,
+    express.static(path.join(adminClientDir, 'public'))
+  );
+  app.all(
+    `/${appConfig.adminUrl}`,
+    (req: express.Request, res: express.Response) => {
+      res.redirect('/admin/');
+    }
+  );
+  app.use(
+    `/${appConfig.adminUrl}/**`,
+    (req: express.Request, res: express.Response) => {
+      handlers.handleAdminPage(res, adminClientDir, tanamConfig.firebaseConfig);
+    }
+  );
+  app.get('/adminnew', (req: express.Request, res: express.Response) =>
+    handlers.handleAdminPage(
+      res,
+      path.join(__dirname, 'admin'),
+      tanamConfig.firebaseConfig
+    )
+  );
   app.get('/manifest.json', handlers.handlePublicDirectoryFileReq);
   app.get('/robots.txt', handlers.handlePublicDirectoryFileReq);
   app.get('/sitemap.xml', handlers.handleSitemapReq);
