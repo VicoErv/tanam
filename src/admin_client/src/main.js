3d9449c2871eb0a40dd1ff5c57b62478c2efc62c
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-09-18 13:08:44 +0800

    [TNM-116] add style for landing page, add conditional user checking

--- src/admin_client/src/main.js
+++ src/admin_client/src/main.js
@@ -8,22 +8,27 @@ const App = () => import('./App.vue');
 import router from './router';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import '@/assets/style.css';
 // import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-  new Vue({
-    router,
-    store,
-    render: h => h(App),
-    created() {
-      if (user) {
-        store
-          .dispatch(AUTO_LOGIN, user)
-          .then(() => router.push('/authenticate'));
+  if (user) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App),
+      created() {
+        if (user) {
+          store
+            .dispatch(AUTO_LOGIN, user)
+            .then(() => router.push('/authenticate'));
+        }
       }
-    }
-  }).$mount('#app');
-  unsubscribe();
+    }).$mount('#app');
+    unsubscribe();
+  } else if (window.location.pathname !== '/admin/') {
+    window.history.replaceState(null, null, '/admin/');
+  }
 });
